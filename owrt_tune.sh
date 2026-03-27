4ee6b22396801418ce9265afd213c1187a78ded8
nocturnal_red <42446434+Nocturnal-ru@users.noreply.github.com>
2026-03-28 00:54:21 +0300

    Update owrt_tune.sh

--- owrt_tune.sh
+++ owrt_tune.sh
@@ -3,33 +3,39 @@
 # Отключаем IPv6 для LAN и WAN
 uci set 'network.lan.ipv6=0'
 uci set 'network.wan.ipv6=0'
-uci set 'dhcp.lan.dhcpv6=disabled'
 
 # Отключаем делегирование IPv6 для LAN
 uci set network.lan.delegate="0"
+
+# Удаляем RA и DHCPv6
+uci -q delete dhcp.lan.dhcpv6
+uci -q delete dhcp.lan.ra
+
+# Удаляем IPv6 ULA Prefix
+uci -q delete network.globals.ula_prefix
+
+# Фильтруем AAAA-записи в DNS через UCI (не через echo в файл!)
+uci set dhcp.@dnsmasq[0].filter_aaaa='1'
+
+# Сохраняем изменения
+uci commit network
+uci commit dhcp
 
 # Отключаем odhcpd
 /etc/init.d/odhcpd disable
 /etc/init.d/odhcpd stop
 
-# Удаляем RA и DHCPv6, чтобы IPv6-адреса не раздавались
-uci -q delete dhcp.lan.dhcpv6
-uci -q delete dhcp.lan.ra
+# Отключаем IPv6 на уровне ядра
+sysctl -w net.ipv6.conf.all.disable_ipv6=1
+sysctl -w net.ipv6.conf.default.disable_ipv6=1
 
-# Удаляем IPv6 ULA Prefix
-uci set network.globals.ula_prefix=''
-uci commit network
+# Делаем постоянным через sysctl.d
+grep -q 'disable_ipv6' /etc/sysctl.d/99-disable-ipv6.conf 2>/dev/null || {
+    echo 'net.ipv6.conf.all.disable_ipv6=1' >> /etc/sysctl.d/99-disable-ipv6.conf
+    echo 'net.ipv6.conf.default.disable_ipv6=1' >> /etc/sysctl.d/99-disable-ipv6.conf
+}
 
-# Применяем изменения одним коммитом
-uci commit network
-uci commit dhcp
-
-# Вносим правку в /etc/config/dhcp для отключения IPv6 DNS-запросов
-if ! grep -q "option filter_aaaa '1'" /etc/config/dhcp; then
-    echo -e "\nconfig dnsmasq\n\toption filter_aaaa '1'" >> /etc/config/dhcp
-fi
-
-# Перезапускаем службы: сначала dnsmasq, потом network
+# Перезапускаем службы
 /etc/init.d/dnsmasq restart
 /etc/init.d/network restart
 
